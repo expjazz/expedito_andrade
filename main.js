20a608016af90982c162fcc0af4a826b93f983a5
Expedito Andrade <expeditojazz@gmail.com>
2020-06-15 11:47:17 -0300

    add js animation for project blur

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+delayedGreeting();
+console.log("Goodbye!");
+
 let animationControllerIn = function (element) {
   document.querySelector(element).classList.add("animation-2");
   document.querySelector(element).classList.remove("title-animation");
@@ -25,9 +32,26 @@ scroll(160, "#title :nth-child(2)");
 scroll(240, "#title :nth-child(3)");
 
 let projectToggler = document.getElementById("proj-one");
+let projOne = document.getElementById("proj-one-show");
 toggler = function () {
-  document.getElementById("container").classList.toggle("blur");
-  document.getElementById("proj-one-show").classList.toggle("d-none");
+  delayedGreeting();
 };
 projectToggler.addEventListener("click", toggler);
 document.getElementById("proj-one-show").addEventListener("click", toggler);
+async function delayedGreeting() {
+  if (Array.from(projOne.classList).includes("proj-animation2")) {
+    document.getElementById("container").classList.toggle("blur");
+    document.getElementById("proj-one-show").classList.toggle("d-none");
+    document.getElementById("proj-one-show").classList.toggle("proj-animation");
+    document
+      .getElementById("proj-one-show")
+      .classList.toggle("proj-animation2");
+  } else if (Array.from(projOne.classList).includes("proj-animation")) {
+    projOne.classList.toggle("proj-animation");
+    projOne.classList.toggle("proj-animation2");
+    document.getElementById("container").classList.toggle("blur");
+    await sleep(2000);
+
+    projOne.classList.toggle("d-none");
+  }
+}

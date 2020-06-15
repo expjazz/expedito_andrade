32e1b72d74f8e0799ef6c44e577c76f80a2d011a
Expedito Andrade <expeditojazz@gmail.com>
2020-06-15 11:03:41 -0300

    Add js for projects. add animation to title

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-document.addEventListener("scroll", (e) => {
-  if (document.documentElement.scrollTop > 80) {
-    document.querySelector("#title :nth-child(1)").classList.add("d-none");
-  } else if (document.documentElement.scrollTop < 80) {
-    document.querySelector("#title :nth-child(1)").classList.remove("d-none");
-  } else if (document.documentElement.scollTop > 81) {
-    document.querySelector("#title :nth-child(2)").classList.add("d-none");
-  } else if (
-    document.documentElement.scrollTop > 80 &&
-    document.documentElement.scrollTop < 100
-  ) {
-    document.querySelector("#title :nth-child(2)").classList.remove("d-none");
-  }
-});
+let animationControllerIn = function (element) {
+  document.querySelector(element).classList.add("animation-2");
+  document.querySelector(element).classList.remove("title-animation");
+};
+let animationControllerOut = function (element) {
+  document.querySelector(element).classList.remove("animation-2");
+  document.querySelector(element).classList.add("title-animation");
+};
+
+let scroll = function (num1, element) {
+  document.addEventListener("scroll", (e) => {
+    if (document.documentElement.scrollTop > num1) {
+      e.stopPropagation();
+
+      animationControllerIn(element);
+    } else if (document.documentElement.scrollTop < num1) {
+      animationControllerOut(element);
+      e.stopPropagation();
+    }
+  });
+};
+
+scroll(80, "#title :nth-child(1)");
+scroll(160, "#title :nth-child(2)");
+scroll(240, "#title :nth-child(3)");
+
+let projectToggler = document.getElementById("proj-one");
+toggler = function () {
+  document.getElementById("container").classList.toggle("blur");
+  document.getElementById("proj-one-show").classList.toggle("d-none");
+};
+projectToggler.addEventListener("click", toggler);
+document.getElementById("proj-one-show").addEventListener("click", toggler);
